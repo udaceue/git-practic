c00e84627304c362581f0548051e93a465d0c4bc
udaceue <rjemik3@gmail.com>
2022-02-02 16:40:24 +0100

    Added node with Express Framework

---
+++ ToDoList-grid/script.js
@@ -0,0 +1,41 @@
+// function kotek(callback){
+//     setTimeout(() => {
+//         console.log("obiekcik a")
+//         const newValue = {
+//             name:"Remik"
+//         }
+//         callback(newValue);
+        
+//     }, 1000);
+//     }
+
+// function showValue(param){
+//     setTimeout(() => {
+//         console.log(param);    
+//         console.log("obiekcik b");
+        
+//     }, 2000);
+    
+
+
+// };
+
+// kotek(showValue);
+
+
+// function initR (x)
+// {
+//     let value = 2
+    
+//     return {
+    
+//         add(x){
+//         value +=x
+//         console.log(value)
+//     }
+// }
+// }
+// const funkcyja = initR();
+// funkcyja.add(2);
+
+console.log("hello");
